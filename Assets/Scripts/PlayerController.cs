88048c9d7086c57f4b72434c975d0bf8fa8c49a4
turgaygunall <153232376+turgaygunall@users.noreply.github.com>
2023-12-09 11:31:04 +0300

    tuuuu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,15 +9,47 @@ public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
     Vector2 moveInput;
+    [SerializeField]
+    private bool _ismoving = false;
+    public bool IsMoving { get
+        {
+            return _ismoving;
 
-    public bool IsMoving { get; private set; }
 
+        }
+        private set
+        {
+            _ismoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+    [SerializeField]
+    private bool _isRunning = false;
+    public bool IsRunning
+    {    
+          
+
+           get
+          {
+
+            return _isRunning;
+        
+          } 
+          set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value );
+        }
+     }
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
+
 
     void FixedUpdate()
     {
@@ -28,4 +61,32 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
         IsMoving = moveInput != Vector2.zero;
     }
+    public void OnRun(InputAction.CallbackContext context)
+    {
+
+        if (context.started)
+        {
+
+            IsRunning = true;
+
+
+
+        }
+        else if (context.canceled)
+        {
+
+            IsRunning = false;
+        }
+
+
+
+
+    }
+
+
+
+
+
 }
+
+
